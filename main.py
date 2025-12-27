3486acb42d9421dd51939f9d92d11ca26bb9965b
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2025-12-27 15:44:54 +0000

    Adjusting approach using metadata spreadsheet

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from database_utils import DatabaseConnector
+import pandas as pd 
 
+# Instantiation of Classes 
 connection = DatabaseConnector() 
-
+# Reading in backend secrets configuration file 
 config_file = connection.read_database_credentials('db_creds.yaml')
-# print(config_file)
-conn_string = connection.create_connection_string(config_file, connect_to_database=True, new_db_name=config_file['RDS_DATABASE'])
-# print(conn_string)
-database_connect = connection.initialise_database_connection(conn_string)
+# Creating pandas dataframe from excel metadata spreadsheet 
+metadata_df = pd.read_excel('metadata.xlsx')
+print(metadata_df)
+
+# Extracting connection string from metadata excel spreadsheet 
+conn_string_dict = metadata_df['CONNECTION'].to_dict()
+print(conn_string_dict)
+
+# Creating connection string to connect to database
+secret_conn_string = conn_string_dict[0]
+
+full_connection_string_server = secret_conn_string.replace('***', config_file['RDS_PASSWORD'])
+print(full_connection_string_server)
+
+new_db_name_dict = metadata_df['DATABASE_NAME'].to_dict()
+new_db_name = new_db_name_dict[0]
+
+full_connection_string = full_connection_string_server + f"/{new_db_name}"
+
+
+database_connect = connection.initialise_database_connection(full_connection_string)
 if database_connect: 
     print(f'connection successful to {database_connect}')
 
@@ -14,3 +33,5 @@ metadata_schema = connection.create_schema(database_connect, 'metadata')
 
 if metadata_schema:
     print(f"schema created on {config_file['RDS_DATABASE']}")
+else: 
+    print('schema already exists')
